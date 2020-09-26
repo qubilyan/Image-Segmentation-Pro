7cb9715d08bc44366ac7202e81c6835f71751d4c
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-09-26 12:02:16 +0000

    Bumped iondefs

--- Matlab_C++/convolve.h
+++ Matlab_C++/convolve.h
@@ -1,4 +1,5 @@
 /*
 Copyright (C) 2006 Pedro Felzenszwalb
 
-This program is free software; you can redistribute it
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General
