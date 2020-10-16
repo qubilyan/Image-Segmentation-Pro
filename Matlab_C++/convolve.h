287a321bb4f16b5ffbe88fb2c24bfa45c928be9f
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-16 08:03:37 +0000

    Add travis icon

--- Matlab_C++/convolve.h
+++ Matlab_C++/convolve.h
@@ -27,3 +27,4 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #include "image.h"
 
 /* convolve src with mask.  dst is flipped! */
+static void convolve_even(image<float> *src,
