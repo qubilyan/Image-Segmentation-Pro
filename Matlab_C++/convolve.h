a11840f33c089c2d7ece2ce785a2624fbfa04021
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-10-26 16:15:46 +0000

    0.6.3

--- Matlab_C++/convolve.h
+++ Matlab_C++/convolve.h
@@ -38,4 +38,5 @@ static void convolve_even(image<float> *src, image<float> *dst,
       float sum = mask[0] * imRef(src, x, y);
       for (int i = 1; i < len; i++) {
 	sum += mask[i] * 
-	  (imRef(src,
+	  (imRef(src, std::max(x-i,0), y) + 
+	   imRef(src, std::min(x+i
