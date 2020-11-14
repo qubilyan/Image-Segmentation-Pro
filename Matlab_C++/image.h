b8937491b7cb0660176c5f9d55c2042542161202
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-11-14 09:24:26 +0000

    Delete default.html

--- Matlab_C++/image.h
+++ Matlab_C++/image.h
@@ -81,3 +81,36 @@ image<T>::image(const int width, const int height, const bool init, const bool a
 template <class T>
 image<T>::~image() {
   delete [] data; 
+  delete [] access;
+}
+
+template <class T>
+void image<T>::setdata(T *data, const bool init) {
+  this->data = data;
+  
+  // initialize row pointers
+  for (int i = 0; i < h; i++)
+    access[i] = data + (i * w);  
+  
+  if (init)
+    memset(data, 0, w * h * sizeof(T));
+}
+
+template <class T>
+void image<T>::init(const T &val) {
+  T *ptr = imPtr(this, 0, 0);
+  T *end = imPtr(this, w-1, h-1);
+  while (ptr <= end)
+    *ptr++ = val;
+}
+
+
+template <class T>
+image<T> *image<T>::copy() const {
+  image<T> *im = new image<T>(w, h, false);
+  memcpy(im->data, data, w * h * sizeof(T));
+  return im;
+}
+
+#endif
+  
