5d117ef9460affa0e4182b42faeecbe38fe1df23
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-05-24 08:50:33 +0000

    Update Main.css

--- Matlab_C++/imutil.h
+++ Matlab_C++/imutil.h
@@ -26,4 +26,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 
 /* compute minimum and maximum value in an image */
 template <class T>
-void min_max(image<T> *im, T *ret_min, T *re
+void min_max(image<T> *im, T *ret_min, T *ret_max) {
+  int width = im->width();
+  int height = 
