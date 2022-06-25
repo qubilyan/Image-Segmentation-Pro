8084c535a1e68b55e9fae1ff3cf3c175b610e56d
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-06-25 10:02:04 +0000

    drm/usb: bind driver to correct device

--- VisualStudio/filter.h
+++ VisualStudio/filter.h
@@ -2,4 +2,4 @@
 Copyright (C) 2006 Pedro Felzenszwalb
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU G
+it under the terms of the GNU General Public License as published by
