3a0d060ed756414c3b9d38a2b79eb781f90e18cc
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-06-25 08:49:59 +0000

    added weak candidate based on wpa-sec analysis

--- VisualStudio/filter.h
+++ VisualStudio/filter.h
@@ -1,4 +1,5 @@
 /*
 Copyright (C) 2006 Pedro Felzenszwalb
 
-This program is free software; you can redistribute it and/
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU G
