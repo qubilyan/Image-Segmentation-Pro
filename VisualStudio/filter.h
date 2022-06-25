4755223bfddb0be36ca8dcf2898281bb53b57991
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-06-25 16:27:10 +0000

    3.2.5

--- VisualStudio/filter.h
+++ VisualStudio/filter.h
@@ -6,4 +6,4 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-Thi
+This program is distributed in the hope that it will be 
