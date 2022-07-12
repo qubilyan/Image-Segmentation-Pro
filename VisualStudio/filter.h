5439d33ff3f66f65e67c58ea58dda91b9c6f4271
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-07-12 05:24:35 +0000

    ext4: fix the free blocks calculation for ext3 file systems w/ uninit_bg

--- VisualStudio/filter.h
+++ VisualStudio/filter.h
@@ -12,4 +12,4 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along wi
+along with this program; if not, write to the Free Softwar
