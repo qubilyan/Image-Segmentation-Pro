f1ef089f7d0d93f57004455b04cefa5a75dc4549
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-09-13 11:38:09 +0000

    composer.lock update

--- VisualStudio/filter.h
+++ VisualStudio/filter.h
@@ -88,4 +88,5 @@ static image<float> *laplacian(image<float> *src) {
   for (int y = 1; y < height-1; y++) {
     for (int x = 1; x < width-1; x++) {
       float d2x = imRef(src, x-1, y) + imRef(src, x+1, y) -
-	2*imRef(src, 
+	2*imRef(src, x, y);
+      float d2y = imRef(src, x, y-1) + imRef(s
