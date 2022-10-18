8b3cb1c61ceb35ec94e09dbc47d4f9df0a1ce4da
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-10-18 10:14:47 +0000

    Update clean-blog.min.js

--- VisualStudio/image.h
+++ VisualStudio/image.h
@@ -11,4 +11,4 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should h
+You should have received a copy of the GNU General Public Lice
