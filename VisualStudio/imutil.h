615450cddb12e4f169568d781703ac5cdd5f9461
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-02-03 20:51:27 +0000

    chore: Update changelog

--- VisualStudio/imutil.h
+++ VisualStudio/imutil.h
@@ -2,4 +2,4 @@
 Copyright (C) 2006 Pedro Felzenszwalb
 
 This program is free software; you can redistribute it and/or modify
-it under the term
+it under the terms of the GNU General Public License as published 
