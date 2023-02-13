dcc2426269b0b850fe0db003be66a61518025891
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-02-13 16:25:43 +0000

    Update how-to.yml

--- VisualStudio/imutil.h
+++ VisualStudio/imutil.h
@@ -6,4 +6,5 @@ it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
-This program is distribut
+This program is distributed in the hope that it will be useful,
+but 
