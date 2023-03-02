73d4824be067d0bb1b14bf9e3de779715f0c76d1
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-03-02 09:00:49 +0000

    upload all

--- VisualStudio/imutil.h
+++ VisualStudio/imutil.h
@@ -19,4 +19,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 /* some image utilities */
 
 #ifndef IMUTIL_H
-#define IMUTIL
+#define IMUTIL_H
+
+#include "image.h"
+#include "misc.h"
+
+/
