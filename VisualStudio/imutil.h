0f1644387512d17d69437b53506aba541f502238
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-04-26 15:41:58 +0000

    Sending translation for Marathi

--- VisualStudio/imutil.h
+++ VisualStudio/imutil.h
@@ -33,4 +33,5 @@ void min_max(image<T> *im, T *ret_min, T *ret_max) {
   T min = imRef(im, 0, 0);
   T max = imRef(im, 0, 0);
   for (int y = 0; y < height; y++) {
-    for (int
+    for (int x = 0; x < width; x++) {
+      T val = imRef(im, x,
