6ebb2c5e6211ef63f1da7017b550bdb99ce575af
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-06-08 08:14:31 +0000

    Rename

--- VisualStudio/imutil.h
+++ VisualStudio/imutil.h
@@ -51,4 +51,6 @@ template <class T>
 image<uchar> *threshold(image<T> *src, int t) {
   int width = src->width();
   int height = src->height();
-  image<uchar> *dst = new image<uch
+  image<uchar> *dst = new image<uchar>(width, height);
+  
+  for (int y = 0; y
